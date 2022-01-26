6e9a4ec9adb3dcd6e504f53ce66444aed71afd10
Gourav Singh <49956754+GouravSingh2580@users.noreply.github.com>
2022-01-27 04:27:42 +0530

    Time: 8 ms (29.77%), Space: 9.7 MB (21.90%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.cpp
+++ 35-search-insert-position/35-search-insert-position.cpp
@@ -1,18 +1,9 @@
 class Solution {
 public:
-    int fc(vector<int>& arr, int k, int x) {
-        vector<pair<int,int> > vpi(arr.size());
-        for(int i = 0; i<arr.size(); i++)vpi[i] = {abs(arr[i]-x), arr[i]};
-        stable_sort(vpi.begin(), vpi.end());
-        vector<int> ans;
-        for(int i=0; i<k; i++)ans.push_back(vpi[i].second);
-        int aa=ans[0];
-        return aa;
-    }
     int searchInsert(vector<int>& n, int t) {
         int low=0;
         int high=n.size()-1;
-        int k=0;
+
         while(low<=high){
             int mid=low+(high-low)/2;
             
@@ -25,8 +16,6 @@ public:
             if(n[mid]>t){
                 high=mid-1;
             }
-            
-            k=mid;
         }
         return low;
     }
