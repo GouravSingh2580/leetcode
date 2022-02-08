3e483456181b2f8fdeb7c0ac797beefaf3cc88a3
Gourav Singh <49956754+GouravSingh2580@users.noreply.github.com>
2022-02-08 15:24:55 +0530

    Time: 3 ms (78.48%), Space: 6.5 MB (98.62%) - LeetHub

---
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        char ch=0;
+        for(auto c:s){
+            ch^=c;
+        }
+        for(auto c:t){
+            ch^=c;
+        }
+        return ch;
+    }
+};
