fb41c340cffcec339143da62a4d241e57d919239
Gourav Singh <49956754+GouravSingh2580@users.noreply.github.com>
2022-02-10 14:35:13 +0530

    Time: 140 ms (34.00%), Space: 41.5 MB (34.96%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        map<int,int>mp;
+        unordered_map<int,int>mp;
         int c=0;
         int s=0;
         mp[0]++;
