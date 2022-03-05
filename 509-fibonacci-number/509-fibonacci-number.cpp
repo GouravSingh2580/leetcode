69399ed4850f6cc0798a547720f86989445c3cad
Gourav Singh <49956754+GouravSingh2580@users.noreply.github.com>
2022-03-06 00:16:10 +0530

    Time: 0 ms (100.00%), Space: 6 MB (16.40%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,8 +1,11 @@
 class Solution {
 public:
+    int dp[10001];
     int fib(int n) {
         if(n<=1) return n;
+        if(dp[n]) return dp[n];
         
-        return fib(n-1)+fib(n-2);
+        dp[n]= fib(n-1)+fib(n-2);
+        return dp[n];
     }
 };
