234d92bac0453e1b5271604ad659949815432610
Gourav Singh <49956754+GouravSingh2580@users.noreply.github.com>
2022-03-30 16:13:47 +0530

    Time: 36 ms (36.85%), Space: 13.3 MB (9.13%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
+    string s;
     int lcs(string& a, string& b, int n, int m, vector<vector<int>>&dp){
-        if(n==0 || m==0) return 0;
+        if(n==0 || m==0) {return 0; cout<<s;}
         
         if(dp[n][m]!=-1) return dp[n][m];
         
         if(a[n-1] ==b[m-1]){
+            s+=a[n-1];
             return dp[n][m]= lcs(a,b,n-1,m-1,dp)+1;
         }
         else{
