771941e09b4c81247b0abd48b415e41281e906d5
Gourav Singh <49956754+GouravSingh2580@users.noreply.github.com>
2022-04-01 23:39:58 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (93.67%) - LeetHub

--- 563-binary-tree-tilt/563-binary-tree-tilt.java
+++ 563-binary-tree-tilt/563-binary-tree-tilt.java
@@ -14,21 +14,18 @@
  * }
  */
 class Solution {
-    public int tilt=0;
-    public int check(TreeNode node)
-    {
-        if(node==null)
-        {
-            return 0;
-        }        
-        int ls=check(node.left);
-        int rs=check(node.right);        
-        int diff=Math.abs(ls-rs);
-        tilt+=diff;        
-        return ls+rs+node.val;
+    public int t=0;
+    public int sol(TreeNode root){
+        if(root==null) return 0;
+        
+        int left=sol(root.left);
+        int right=sol(root.right);
+        
+        t+=Math.abs(left-right);
+        return(left+right+root.val);
     }
-    public int findTilt(TreeNode node) {
-        check(node);
-        return tilt;
+    public int findTilt(TreeNode root) {
+        sol(root);
+        return t;
     }
 }
