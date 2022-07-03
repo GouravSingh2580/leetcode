40582c9546ac83acb0ed9b2ad7550b795e81e3db
Gourav Singh <49956754+GouravSingh2580@users.noreply.github.com>
2022-07-03 20:00:36 +0530

    Time: 16 ms (6.94%), Space: 9.5 MB (5.96%) - LeetHub

---
+++ 198-house-robber/198-house-robber.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int dp(vector<int>v,int n,vector<int>&ap){
+        if(n<0) return 0;
+        if(n==0) return v[0];
+        if(ap[n]!=-1) return ap[n];
+        int x=v[n]+dp(v,n-2,ap);
+        int y=dp(v,n-1,ap);
+        return ap[n]=max(x,y);
+    }
+    int rob(vector<int>& n) {
+        vector<int>ap(size(n)+1,-1);
+        return dp(n,size(n)-1,ap);
+    }
+};
