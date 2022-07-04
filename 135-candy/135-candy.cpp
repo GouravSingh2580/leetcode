8d53222c2ba07c52d778a6de711794b8b061cfbe
Gourav Singh <49956754+GouravSingh2580@users.noreply.github.com>
2022-07-04 23:04:54 +0530

    Time: 36 ms (43.40%), Space: 18.2 MB (23.59%) - LeetHub

---
+++ 135-candy/135-candy.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int candy(vector<int>& r) {
+        int ans = 0, n= r.size();
+        vector<int>l(n,1);
+        for(int i=1; i<n; i++){
+            if(r[i]>r[i-1]) l[i]=l[i-1]+1;
+        }
+        vector<int> ri(n,1);
+        for(int i=n-2; ~i; i--){
+            if(r[i]>r[i+1]) ri[i] = ri[i+1]+1;
+        }
+        for(int i=0; i<n; i++){
+            ans += max(l[i], ri[i]);
+        }
+        return ans;
+    }
+};
