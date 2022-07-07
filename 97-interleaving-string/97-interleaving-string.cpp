ed4698daf216077fe648689b15c0ad3fa75909ae
Gourav Singh <49956754+GouravSingh2580@users.noreply.github.com>
2022-07-07 12:42:01 +0530

    Time: 188 ms (5.02%), Space: 53.8 MB (5.76%) - LeetHub

---
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    map<string,bool> mp;
+    bool isInterleave(string s1, string s2, string s3) {
+        if(s1.empty() && s2.empty() && s3.empty()) return true;
+        if(s3.empty()) return false;
+        
+        string str=s1+"|"+s2+"|"+s3;
+        if(mp.count(str)) return mp[str];
+        
+        bool f= (!s3.empty() && s1[0]==s3[0]) && isInterleave(s1.substr(1),s2,s3.substr(1));
+        bool s= (!s3.empty() && s2[0]==s3[0]) && isInterleave(s1,s2.substr(1),s3.substr(1));
+        
+        return mp[str]=f or s;
+    }
+};
