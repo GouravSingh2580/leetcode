debe32f3d0ea597615fda1a199fe05c92773405f
Gourav Singh <49956754+GouravSingh2580@users.noreply.github.com>
2022-07-07 14:58:02 +0530

    Time: 153 ms (5.02%), Space: 52.1 MB (5.94%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,16 +1,25 @@
 class Solution {
 public:
     unordered_map<string,bool> mp;
-    bool isInterleave(string s1, string s2, string s3) {
+    bool abc(string s1, string s2, string s3,int i,int j,int k)
+    {
         if(s1.empty() && s2.empty() && s3.empty()) return true;
         if(s3.empty()) return false;
         
         string str=s1+"|"+s2+"|"+s3;
         if(mp.count(str)) return mp[str];
         
-        bool f= (!s3.empty() && s1[0]==s3[0]) && isInterleave(s1.substr(1),s2,s3.substr(1));
-        bool s= (!s3.empty() && s2[0]==s3[0]) && isInterleave(s1,s2.substr(1),s3.substr(1));
+        bool a;
+        a=(!s3.empty() && s1[0]==s3[0]) && abc(s1.substr(1),s2,s3.substr(1),++i,j,++k);
+        bool b;
+        b=(!s3.empty() && s2[0]==s3[0]) && abc(s1,s2.substr(1),s3.substr(1),i,++j,++k);
         
-        return mp[str]=f or s;
+        return mp[str]= a or b;
+    }
+    bool isInterleave(string s1, string s2, string s3) {
+        if(s1.size()+s2.size()!=s3.size()) return false;
+        if(s1.empty() or s2.empty() && s1+s2==s3) return true;
+        else if(s1.empty() or s2.empty() && s1+s2!=s3) return false;
+        else return abc(s1,s2,s3,0,0,0);
     }
 };
