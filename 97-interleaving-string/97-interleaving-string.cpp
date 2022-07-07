1c07da3d566fc2fb2d4914f5f742e168090cd5ec
Gourav Singh <49956754+GouravSingh2580@users.noreply.github.com>
2022-07-07 15:51:56 +0530

    Time: 4 ms (73.94%), Space: 7.2 MB (43.01%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
-    unordered_map<string,bool> mp;
-    bool abc(string s1, string s2, string s3,int i,int j,int k)
-    {
-        if(s1.empty() && s2.empty() && s3.empty()) return true;
-        if(s3.empty()) return false;
-        
-        string str=s1+"|"+s2+"|"+s3;
-        if(mp.count(str)) return mp[str];
-        
-        bool a;
-        a=(!s3.empty() && s1[0]==s3[0]) && abc(s1.substr(1),s2,s3.substr(1),++i,j,++k);
-        bool b;
-        b=(!s3.empty() && s2[0]==s3[0]) && abc(s1,s2.substr(1),s3.substr(1),i,++j,++k);
-        
-        return mp[str]= a or b;
-    }
+    
     bool isInterleave(string s1, string s2, string s3) {
-        if(s1.size()+s2.size()!=s3.size()) return false;
-        if(s1.empty() or s2.empty() && s1+s2==s3) return true;
-        else if(s1.empty() or s2.empty() && s1+s2!=s3) return false;
-        else return abc(s1,s2,s3,0,0,0);
+        vector<vector<int> > memo(s1.size()+5, vector<int>(s2.size()+5, 0));
+        if (s1.size() + s2.size() != s3.size()) return false;
+        return helper(s1, s2, s3, 0, 0, memo);
+    }
+    
+    bool helper(string& a, string &b, string &c, int i, int j,
+                vector<vector<int> >& memo) {
+        int k = i + j;
+        if (k>=c.size()) return true;
+        if (memo[i][j] == 1) {
+            return false;
+        }
+        memo[i][j] = 1;
+        if (a[i] == c[k] && b[j] != c[k]) {
+            return helper(a, b, c, i+1, j, memo);
+        } 
+        if (b[j] == c[k] && a[i] != c[k]){
+            return helper(a, b, c, i, j+1, memo);
+        } 
+        if (a[i] == c[k] && b[j] == c[k]) {
+            return helper(a, b, c, i+1, j, memo) 
+                || helper(a, b, c, i, j+1, memo);
+        } 
+        return false;
     }
 };
