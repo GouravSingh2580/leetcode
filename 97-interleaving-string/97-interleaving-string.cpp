d9bcf2d86c9363cfc041c1ffd5d47adf0f141dfd
Gourav Singh <49956754+GouravSingh2580@users.noreply.github.com>
2022-07-07 19:07:16 +0530

    Time: 88 ms (13.48%), Space: 35.2 MB (13.08%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
-    
     bool dp(string s1,string s2,string s3,int i,int j,int k,vector<vector<int >>&m){
         
         if(m[i][j]!=-1) return m[i][j];
         
-        if(i==s1.length() && j==s2.length() && k==s3.length()) return true;
+        if(i==s1.size() && j==s2.size() && k==s3.size()) return true;
 
-        bool x=(i<s1.length()) && (s1[i]== s3[k]) && dp(s1,s2,s3,i+1,j,k+1,m);
-        bool y=(j<s2.length()) && (s2[j]== s3[k]) && dp(s1,s2,s3,i,j+1,k+1,m);
+        bool x=(i<s1.size()) && (s1[i]== s3[k]) && dp(s1,s2,s3,i+1,j,k+1,m);
+        bool y=(j<s2.size()) && (s2[j]== s3[k]) && dp(s1,s2,s3,i,j+1,k+1,m);
         
         return m[i][j]=x or y;
     }
     bool isInterleave(string s1,string s2,string s3){
-        int a=s1.length(),b=s2.length();
+        int a=s1.size(),b=s2.size();
         vector<vector<int >>m(a+1,vector<int>(b+1,-1));
         return dp(s1,s2,s3,0,0,0,m);
     }
