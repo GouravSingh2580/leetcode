1275485640ac3067d0fe2e0e510579a0531ee55b
Gourav Singh <49956754+GouravSingh2580@users.noreply.github.com>
2022-07-07 19:04:54 +0530

    Time: 76 ms (15.88%), Space: 35.3 MB (12.73%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -2,19 +2,12 @@ class Solution {
 public:
     
     bool isInterleave_helper(string s1,string s2,string s3,int i,int j,int k,vector<vector<int >>&m){
-    if(m[i][j]!=-1){
-        return m[i][j];
-   }
-    if(i== s1.length()&&j== s2.length()&&k==s3.length()){
-        return true;
-    }
-    bool x=false,y=false;
-    if(i<s1.length()){
-        if(s1[i]== s3[k]) x=isInterleave_helper(s1,s2,s3,i+1,j,k+1,m);
-    }
-       if(j<s2.length()){
-            if(s2[j]== s3[k]) y=isInterleave_helper(s1,s2,s3,i,j+1,k+1,m);         
-       }
+    if(m[i][j]!=-1) return m[i][j];
+    if(i== s1.length()&&j== s2.length()&&k==s3.length()) return true;
+        
+    bool x=(i<s1.length()) && (s1[i]== s3[k]) && isInterleave_helper(s1,s2,s3,i+1,j,k+1,m);
+    
+    bool y=(j<s2.length()) && (s2[j]== s3[k]) && isInterleave_helper(s1,s2,s3,i,j+1,k+1,m);
     return m[i][j]=x or y;
 }
 bool isInterleave(string s1,string s2,string s3){
