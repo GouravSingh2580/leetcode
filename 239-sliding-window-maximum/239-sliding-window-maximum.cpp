4def9a510b2a231901e7d2ed246e0b4e5070db59
Gourav Singh <49956754+GouravSingh2580@users.noreply.github.com>
2022-07-09 17:29:43 +0530

    Time: 1392 ms (5.01%), Space: 212.1 MB (5.28%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -4,8 +4,8 @@ public:
         vector<int> v;
         set<pair<int, int>> s;
         for(int i = 0; i < n.size(); i ++){
-            s.insert(pair<int, int>(n[i], i));
-            if(s.size() > k) s.erase(pair<int, int>(n[i - k], i - k));
+            s.insert({ n[i], i });
+            if(s.size() > k) s.erase({ n[i-k], i-k });
             if(s.size() == k) v.push_back(s.rbegin()->first); 
         }
         return v;
