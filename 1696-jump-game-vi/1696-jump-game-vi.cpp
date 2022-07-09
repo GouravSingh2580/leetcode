493424a4de53fbc2a3822ada0625e48506c9f152
Gourav Singh <49956754+GouravSingh2580@users.noreply.github.com>
2022-07-09 15:53:46 +0530

    Time: 283 ms (59.49%), Space: 88 MB (52.81%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.cpp
+++ 1696-jump-game-vi/1696-jump-game-vi.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     int dp[100001];
-    int dfs(vector<int> &a, int k, int ind) {
-        if(dp[ind]!=INT_MIN/2) return dp[ind];
-        int t=ind-1,val=a[ind-1];
-        for(int i=ind-1;i>=max(0,ind-k);i--){
+    int dfs(vector<int> &a, int k, int n) {
+        if(dp[n]!=INT_MIN/2) return dp[n];
+        int t=n-1,val=a[n-1];
+        for(int i=n-1;i>=max(0,n-k);i--){
             //nonpositve is always the best option
-            if(a[i]>=0) return dp[ind]=dfs(a,k,i)+a[ind];
+            if(a[i]>=0) return dp[n]=dfs(a,k,i)+a[n];
             //if a value comes early but with a greater value
             //it is always better to choose this one
             if(a[i]>=val) val=a[i],t=i;
         }
         int res=INT_MIN;
-        for(int i=t;i>=max(0,ind-k);i--)
+        for(int i=t;i>=max(0,n-k);i--)
             res=max(res,dfs(a,k,i));
-        return dp[ind]=res+a[ind];
+        return dp[n]=res+a[n];
     }
     int maxResult(vector<int>& a, int k) {
         for(int i=0;i<100001;i++) dp[i]=INT_MIN/2;
