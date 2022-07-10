7602d36887611cf7a841edcdfc91113fd0977ab8
Gourav Singh <49956754+GouravSingh2580@users.noreply.github.com>
2022-07-10 17:20:44 +0530

    Time: 27 ms (5.26%), Space: 13.8 MB (51.95%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -1,21 +1,14 @@
-// time O(n) +  space O(n)
-// 使用了记忆化递归，避免了重复计算。用于记忆化的数组本身占用O(n)时间复杂度
-
-// 题目定义为：跳到第n阶台阶需要花多少钱
 class Solution {
- private:
-  int recursion(vector<int> &cost, vector<int> &memo, int n) {
-      if (n <= 1) return 0; // 可以理解为，从未知的地方跳到第0或者第1阶台阶不花钱
-      if (memo[n]) return memo[n];
-      memo[n] = min(recursion(cost, memo, n - 1) + cost[n - 1], recursion(cost, memo, n - 2) + cost[n - 2]);
-	  // 跳到第n阶台阶的价钱 = min(跳到第n-1阶台阶价钱 + 第n-1阶台阶自身的价钱，或者跳到第n-2阶台阶价钱 + 第n-2阶台阶自身的价钱)
-      return memo[n];
-  }
-
- public:
-  int minCostClimbingStairs(vector<int> &cost) {
-      int n = cost.size();
-      vector<int> memo(n + 1, 0);
-      return recursion(cost, memo, n);
-  }
+private:
+    int mccs(vector<int>&c, int n, vector<int>&dp){
+        if(n<=1) return 0;
+        if(dp[n]) return dp[n];
+        dp[n]= min(mccs(c,n-2,dp) +c[n-2], mccs(c,n-1,dp) +c[n-1]);
+        return dp[n];
+    }
+public:
+    int minCostClimbingStairs(vector<int>& c) {
+        vector<int>dp(size(c)+1,0);
+        return mccs(c,size(c),dp);
+    }
 };
