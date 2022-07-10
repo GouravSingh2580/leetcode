79f0a141bb9837406f023e7de4b52d4d9e280631
Gourav Singh <49956754+GouravSingh2580@users.noreply.github.com>
2022-07-10 16:52:33 +0530

    Time: 14 ms (32.00%), Space: 13.8 MB (28.79%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -1,15 +1,21 @@
+// time O(n) +  space O(n)
+// 使用了记忆化递归，避免了重复计算。用于记忆化的数组本身占用O(n)时间复杂度
+
+// 题目定义为：跳到第n阶台阶需要花多少钱
 class Solution {
-public:
-    int dp[10001];
-    int solve(vector<int>&arr,int s){
-        if(s<0){
-            return 0;
-        }
-        if(dp[s]) return dp[s];
-        dp[s]= arr[s] + min(solve(arr,s-1), solve(arr,s-2));
-        return dp[s];
-    }
-    int minCostClimbingStairs(vector<int>& cost) {
-        return min( solve(cost,cost.size()-1), solve(cost,cost.size()-2) );
-    }
+ private:
+  int recursion(vector<int> &cost, vector<int> &memo, int n) {
+      if (n <= 1) return 0; // 可以理解为，从未知的地方跳到第0或者第1阶台阶不花钱
+      if (memo[n]) return memo[n];
+      memo[n] = min(recursion(cost, memo, n - 1) + cost[n - 1], recursion(cost, memo, n - 2) + cost[n - 2]);
+	  // 跳到第n阶台阶的价钱 = min(跳到第n-1阶台阶价钱 + 第n-1阶台阶自身的价钱，或者跳到第n-2阶台阶价钱 + 第n-2阶台阶自身的价钱)
+      return memo[n];
+  }
+
+ public:
+  int minCostClimbingStairs(vector<int> &cost) {
+      int n = cost.size();
+      vector<int> memo(n + 1, 0);
+      return recursion(cost, memo, n);
+  }
 };
