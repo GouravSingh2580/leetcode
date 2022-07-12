0698a06992af5175ae0e5212ad4a2a86decab230
Gourav Singh <49956754+GouravSingh2580@users.noreply.github.com>
2022-07-12 17:29:28 +0530

    Time: 330 ms (54.78%), Space: 10.2 MB (30.06%) - LeetHub

--- 473-matchsticks-to-square/473-matchsticks-to-square.cpp
+++ 473-matchsticks-to-square/473-matchsticks-to-square.cpp
@@ -1,26 +1,19 @@
 class Solution {
 public:
-    bool util(vector<int>& m, int s1, int s2, int s3, int s4, int n){
-        if(s1==0 && s2==0 && s3==0 && s4==0){
-            return true;
-        }
-        if(s1<0 || s2<0 || s3<0 || s4<0){
-            return false;
-        }
-        if(util(m,s1-m[n],s2,s3,s4,n-1) || util(m,s1,s2-m[n],s3,s4,n-1) 
-           || util(m,s1,s2,s3-m[n],s4,n-1) || util(m,s1,s2,s3,s4-m[n],n-1)){
-            return true;
-        }
-        return false;
+    bool dp(vector<int>&m, int n, int a, int b, int c, int d){
+        if(a==0 && b==0 && c==0 && d==0) return true;
+        if(a<0 or b<0 or c<0 or d<0) return false;
+        
+        bool f=(dp(m,n-1,a-m[n],b,c,d) or dp(m,n-1,a,b-m[n],c,d) or dp(m,n-1,a,b,c-m[n],d) or dp(m,n-1,a,b,c,d-m[n]));
+        
+        if(f) return true;
+        else return false;
     }
     bool makesquare(vector<int>& m) {
-        long long s=0;
         sort(begin(m),end(m));
-        for(int i=0;i<m.size();i++)
-            s+=m[i];
-        if(s%4!=0)
-            return false;
-        else
-            return util(m,s/4,s/4,s/4,s/4,m.size()-1);
+        long long s=0;
+        for(auto i:m) s+=i;
+        if(s%4) return false;
+        else return dp(m,size(m)-1,s/4,s/4,s/4,s/4);
     }
 };
