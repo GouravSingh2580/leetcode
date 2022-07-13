e10a1192a35845ab0ee9824598f5c10d9b2f6683
Gourav Singh <49956754+GouravSingh2580@users.noreply.github.com>
2022-07-13 22:51:16 +0530

    Time: 7 ms (12.25%), Space: 9.7 MB (5.92%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int dp(vector<int>v,int n,vector<int>&ap){
         if(n<0) return 0;
-        if(n==0) return v[0];
+        // if(n==0) return v[0];
         if(ap[n]!=-1) return ap[n];
         int x=v[n]+dp(v,n-2,ap);
         int y=dp(v,n-1,ap);
