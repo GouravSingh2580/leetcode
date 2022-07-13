4367d26b7820c2b7ea6622d700c4d4a0c2856acc
Gourav Singh <49956754+GouravSingh2580@users.noreply.github.com>
2022-07-14 01:32:05 +0530

    Time: 479 ms (5.01%), Space: 95.9 MB (5.13%) - LeetHub

---
+++ 322-coin-change/322-coin-change.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int ans(vector<int>& arr, int w, int n, vector<vector<int>>& dp){
+        if(n == 0 || w == 0) return (w == 0 )? dp[w][n] = 0 : dp[w][n] =1e9;
+        if(dp[w][n] != -1) return dp[w][n];
+        if(arr[n-1] > w) return dp[w][n] =ans(arr, w, n-1, dp);
+        else return dp[w][n] = min(ans(arr, w - arr[n-1], n, dp) + 1, ans(arr, w, n-1, dp));
+    }
+    int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
+        vector<vector<int>> dp(amount+1, vector<int>(n+1,-1));
+        return ans(coins, amount, n, dp) == 1e9 ? -1 : ans(coins, amount, n, dp); 
+    }
+};
