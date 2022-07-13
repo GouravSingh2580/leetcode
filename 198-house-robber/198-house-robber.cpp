3399e5fb008b5c3289e23ebedeaf573e05e9a118
Gourav Singh <49956754+GouravSingh2580@users.noreply.github.com>
2022-07-13 23:19:50 +0530

    Time: 7 ms (12.25%), Space: 7.8 MB (24.74%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -4,10 +4,10 @@ public:
     {
         if(i>=a.size()) return 0;
         if(dp[i]!=-1) return dp[i];
-        return dp[i]= max(call(a,i-1,dp),a[i]+call(a,i-2,dp));
+        return dp[i]= max(call(a,i+1,dp),a[i]+call(a,i+2,dp));
     }
     int rob(vector<int>& nums) {
         vector<int>dp(size(nums)+1,-1);
-        return call(nums,size(nums)-1,dp);
+        return call(nums,0,dp);
     }
 };
