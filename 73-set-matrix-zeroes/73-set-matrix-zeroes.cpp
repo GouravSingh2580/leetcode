d5f3c8586432812a1fe87a37fb535bac12b43491
Gourav Singh <49956754+GouravSingh2580@users.noreply.github.com>
2022-07-14 00:56:22 +0530

    Time: 16 ms (82.83%), Space: 13.3 MB (18.74%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,27 +1,30 @@
+/* We use a magic number and hope that it isn't present in the input */
+#define MAGIC_NUMBER    0xdeadbeef
+
 class Solution {
 public:
+    /* Search for zeros, setting rows and columns to our magic number */
     void setZeroes(vector<vector<int>>& matrix) {
-        queue<pair<int,int>> q;
-        int row = matrix.size(), col = matrix[0].size();
-        
-        for(int i=0;i<row;++i){
-            for(int j=0;j<col;++j){
-                if(matrix[i][j]==0) q.push({i,j});
+        for (size_t i = 0; i < matrix.size(); i++) {
+            for (size_t j = 0; j < matrix[i].size(); j++) {
+                if (matrix[i][j] == 0) {
+                    for (size_t k = 0; k < matrix.size(); k++) {
+                        matrix[k][j] = matrix[k][j] == 0 ? 0 : MAGIC_NUMBER;
+                    }
+                    for (size_t k = 0; k < matrix[i].size(); k++) {
+                        matrix[i][k] = matrix[i][k] == 0 ? 0 : MAGIC_NUMBER;
+                    }
+                }
             }
         }
         
-        while(!q.empty()){
-            int x = q.front().first;
-            int y = q.front().second;
-            q.pop();
-            for(int r=0; r<row; ++r){
-                matrix[r][y]=0;
-            }
-            for(int c=0; c<col; ++c){
-                matrix[x][c]=0;
+        /* Now set all elements equal to our magic number to 0 */
+        for (size_t i = 0; i < matrix.size(); i++) {
+            for (size_t j = 0; j < matrix[i].size(); j++) {
+                if (matrix[i][j] == MAGIC_NUMBER) {
+                    matrix[i][j] = 0;
+                }
             }
         }
-        
-        return;
     }
 };
