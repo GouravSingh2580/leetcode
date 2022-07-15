fa8dd6652fa635025f6029892fc7c7ed1633cd9d
Gourav Singh <49956754+GouravSingh2580@users.noreply.github.com>
2022-07-15 22:17:46 +0530

    Time: 43 ms (21.39%), Space: 23.3 MB (48.63%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -1,30 +1,51 @@
 class Solution {
 public:
-    int dp(vector<vector<int>>&grid,int i, int j, int &c){
-        if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size()) return 0;
-        if(grid[i][j]==0) return 0;
-        grid[i][j]=0;
-        
-        if(dp(grid,i-1,j,c)) c++;
-        if(dp(grid,i+1,j,c)) c++;
-        if(dp(grid,i,j+1,c)) c++;
-        if(dp(grid,i,j-1,c)) c++;
-        
-        return 1;
+    int call(vector<vector<int>> &a,int i,int j,int n,int m)
+    {
+        if(i>=n||j>=m||a[i][j]==0) return 1;
+        int z=0,b=0,c=0,d=0;
+        if(j+1<m && a[i][j+1]==1){
+            a[i][j]=0;
+            z=call(a,i,j+1,n,m);
+        }
+        if(i+1<n && a[i+1][j]==1){
+            a[i][j]=0;
+            b=call(a,i+1,j,n,m);
+        }
+        if(i-1>=0 && a[i-1][j]==1){
+            a[i][j]=0;
+            c=call(a,i-1,j,n,m);
+        }
+        if(j-1>=0 && a[i][j-1]==1){
+            a[i][j]=0;
+            d=call(a,i,j-1,n,m);
+        }
+        // else{
+            a[i][j]=0;
+            return 1+z+b+c+d;
+        // }
     }
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        int m=grid.size();
-        int n=grid[0].size();
-        int ans=0;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                int c=1;
-                if(grid[i][j]){
-                    dp(grid,i,j,c);
-                    ans=max(ans,c);
+        int i,j,z,k=0,n=grid.size(),m=grid[0].size();
+        // cout<<n<<" "<<m<<"\n";
+        for(i=0;i<n;++i)
+        {
+            for(j=0;j<m;++j)
+            {
+                if(grid[i][j]==1)
+                {
+                    z=1;
+                    z=call(grid,i,j,n,m);
+                    cout<<z<<" "<<i+1<<" "<<j+1<<"\n";
+                    // if(n!=m) 
+                        // ++z;
+                    if(k<z)
+                    {
+                        k=z;
+                    }
                 }
             }
         }
-        return ans;
+        return k;
     }
 };
