774fbdd19b5887bc2df063f814ee64f3be7c0f4e
Gourav Singh <49956754+GouravSingh2580@users.noreply.github.com>
2022-07-17 20:30:00 +0530

    Time: 7 ms (81.79%), Space: 13.9 MB (25.37%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -2,25 +2,19 @@ class Solution {
     int dp[10001];
     int n; vector<int> arr;
     int rec(int level){
-        if(level == n)
-            return 0;
+        if(level == n) return 0;
         
-        if(dp[level]!= -1)
-            return dp[level];
+        if(dp[level]) return dp[level];
         int ans = -1;
-        //computatuion
-        if(level+1 <= n){
-            ans = arr[level]+rec(level+1);
-        }
-        if(level+2 <= n){
-            ans =  min(ans,arr[level]+rec(level+2));
-        }
+        
+        if(level+1 <= n) ans = arr[level]+rec(level+1);
+        if(level+2 <= n) ans =  min(ans,arr[level]+rec(level+2));
         
         return dp[level] = ans;
     }
     public:
     int minCostClimbingStairs(vector<int>& cost) {
-        memset(dp,-1,sizeof(dp));
+        memset(dp,0,sizeof(dp));
         n= cost.size();
         arr.resize(n);
         arr =cost;
