76321fbdc09854789c6aa715fe1d86be492e0e4f
Gourav Singh <49956754+GouravSingh2580@users.noreply.github.com>
2022-07-18 15:57:13 +0530

    Time: 1543 ms (5.14%), Space: 33.1 MB (8.06%) - LeetHub

--- 645-set-mismatch/645-set-mismatch.cpp
+++ 645-set-mismatch/645-set-mismatch.cpp
@@ -4,13 +4,11 @@ public:
         int maxe=n.size();
         vector<int>dp {0,1,maxe+1,0,maxe};
         int xoz=dp[((maxe-1)%4) +1];
-
-        set<int>st;
         
+        set<int>st;
         for(auto i:n) st.insert(i);
         
         for(auto i:st) xoz^=i;
-        
         for(auto i:n){
             int c=count(begin(n),end(n),i);
             if(c>1) return {i,xoz};
