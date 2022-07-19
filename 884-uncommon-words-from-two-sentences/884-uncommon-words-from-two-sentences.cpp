5d9640c7da40b93a70136312b925d6eded146101
Gourav Singh <49956754+GouravSingh2580@users.noreply.github.com>
2022-07-19 15:23:40 +0530

    Time: 6 ms (26.76%), Space: 6.9 MB (23.92%) - LeetHub

--- 884-uncommon-words-from-two-sentences/884-uncommon-words-from-two-sentences.cpp
+++ 884-uncommon-words-from-two-sentences/884-uncommon-words-from-two-sentences.cpp
@@ -11,10 +11,7 @@ public:
     vector<string> uncommonFromSentences(string s1, string s2) {
         map<string,int>mp1=string_to_map(s1);
         map<string,int>mp2=string_to_map(s2);
-        
-        
-        // vector<string>v1=string_tovec(s1);
-        // vector<string>v2=string_tovec(s2);
+
         vector<string>v;
         for(auto i:mp1){
             if(mp2.count(i.first)<1 && i.second==1) v.push_back(i.first);
