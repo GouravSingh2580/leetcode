189f2e277dc42932c204ef2c6c22e09569de7de7
Gourav Singh <49956754+GouravSingh2580@users.noreply.github.com>
2022-07-20 23:36:05 +0530

    Time: 2950 ms (13.66%), Space: 15.6 MB (75.71%) - LeetHub

--- 792-number-of-matching-subsequences/792-number-of-matching-subsequences.py
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.py
@@ -1,12 +1,18 @@
 class Solution:
     def numMatchingSubseq(self, s: str, words: List[str]) -> int:
+        dp1={}
         
+        for i in s:
+            dp1[i]=True
+        print(dp1)
         dp = {s:True}
         
         def isSub(s,word):
             if word in dp:
                 return dp[word]
             j = 0
+            if word[j] not in dp1:
+                return False
             for i in range(len(s)):
                 if s[i] == word[j]:
                     j += 1
