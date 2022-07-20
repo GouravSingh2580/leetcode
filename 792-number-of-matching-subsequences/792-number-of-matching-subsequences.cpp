5d7766b7a0120f51c6e862d31329e09d50413d74
Gourav Singh <49956754+GouravSingh2580@users.noreply.github.com>
2022-07-20 22:22:25 +0530

    Time: 958 ms (15.24%), Space: 48.3 MB (58.35%) - LeetHub

--- 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
@@ -1,64 +1,52 @@
 class Solution {
 public:
-    // bool iss(string& s1, string& s2)
-    // {
-    //     int n = s1.length(), m = s2.length();
-    //     int i = 0, j = 0;
-    //     while (i < n && j < m) {
-    //         if (s1[i] == s2[j])
-    //             i++;
-    //         j++;
-    //     }
-    //     return i == n;
-    // }
-    int numMatchingSubseq(string s, vector<string>& w) {
-        map<char,vector<int>>mp;
-        int j=0;
-        for(auto i:s) mp[i].push_back(j++);
+    int numMatchingSubseq(string s, vector<string>& words) {
+        map<int,int> a;
+        int i,j,k,ans=0;
+        for(i=0;i<s.size();++i)
+        {
+            ++a[s[i]-'a'];
+        }
+        vector<int> z[27];
+        for(i=0;i<s.size();++i)
+        {
+            z[s[i]-'a'].push_back(i);
+        }
         
-        int c=0;
-        for(int i=0;i<w.size();i++){
-            string t=w[i];
-            int latest_idx=-1;
-            for(int j=0;j<t.size();j++){
-                auto it=upper_bound(begin(mp[t[j]]),end(mp[t[j]]),latest_idx); //all greater values than latest_idx
-                
-                if(it==mp[t[j]].end()) break;
-                
-                latest_idx=*it;
-                if(j==t.size()-1) c++;
+        for(i=0;i<words.size();++i)
+        {
+            k=-1;
+            int flag=0;
+            for(j=0;j<words[i].size();++j)
+            {
+                if(a[words[i][j]-'a']==0)
+                {
+                    flag=1;
+                    break;
+                }
+                else
+                {
+                    int p=k;
+                    for(int x=0;x<z[words[i][j]-'a'].size();++x)
+                    {
+                        if(k<z[words[i][j]-'a'][x])
+                        {
+                            k=z[words[i][j]-'a'][x];
+                            break;
+                        }
+                    }
+                    if(p==k)
+                    {
+                        flag=1;
+                        break;
+                    }
+                }
+            }
+            if(flag==0)
+            {
+                ++ans;
             }
         }
-        return c;
+        return ans;
     }
-    // unordered_set<string> st;
-    // unordered_map<string,int>mp;
-    // void subsequence(string str)
-    // {
-    //     int i;
-    //     for (i = 0; i < str.length(); i++) {
-    //         for (int j = str.length(); j > i; j--) {
-    //             string sub_str = str.substr(i, j);
-    //             // st.insert(sub_str);
-    //             mp[sub_str]++;
-    //             for (int k = 1; k < sub_str.length(); k++) {
-    //                 string sb = sub_str;
-    //                 sb.erase(sb.begin() + k);
-    //                 subsequence(sb);
-    //             }
-    //         }
-    //     }
-    // }
-    
-    
-    // void su(string input, string output)
-    // {
-    //     if (input.empty()) {
-    //         mp[output]++;
-    //         // v.push_back(output);
-    //         return;
-    //     }
-    //     su(input.substr(1), output + input[0]);
-    //     su(input.substr(1), output);
-    // }
 };
