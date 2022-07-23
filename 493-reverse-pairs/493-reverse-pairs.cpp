15935ad92abaf1151e8a6832d02917b4a31a26a6
Gourav Singh <49956754+GouravSingh2580@users.noreply.github.com>
2022-07-23 18:46:34 +0530

    Time: 2055 ms (5.05%), Space: 104.2 MB (65.01%) - LeetHub

--- 493-reverse-pairs/493-reverse-pairs.cpp
+++ 493-reverse-pairs/493-reverse-pairs.cpp
@@ -1,4 +1,10 @@
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
 class Solution {
+public:
+typedef tree<int, null_type, less_equal<int>,rb_tree_tag,tree_order_statistics_node_update> pbd;
     int mergesort(vector<int>&nums,int lo,int hi){
         if(lo>=hi) return 0;
         int mid = (hi+1-lo)/2+lo;
@@ -11,8 +17,16 @@ class Solution {
         inplace_merge(nums.begin()+lo,nums.begin()+mid,nums.begin()+hi+1);
         return cnt;
     }
-    public:
-    int reversePairs(vector<int>& nums) {
-        return mergesort(nums,0,nums.size()-1);
+    int reversePairs(vector<int>& n) {
+        pbd _set;
+        int c=0;
+        for(int i=n.size()-1;i>=0;i--){
+            long long t=n[i];
+            if(t%2LL) t++;
+            c+=_set.order_of_key(t/2LL);
+            cout<<_set.order_of_key(t/2LL)<<endl;
+            _set.insert(n[i]);
+        }
+        return c;
     }
 };
