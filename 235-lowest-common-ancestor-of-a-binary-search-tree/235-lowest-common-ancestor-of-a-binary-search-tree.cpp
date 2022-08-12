275c9a52f8589fa886a1cb7964028409e2071725
Gourav Singh <49956754+GouravSingh2580@users.noreply.github.com>
2022-08-12 15:45:33 +0530

    Time: 35 ms (84.39%), Space: 23.3 MB (63.86%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -11,7 +11,7 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        set<TreeNode*> st;
+        multiset<TreeNode*> st;
         TreeNode* t=root,* lst;
         while(1){
             st.insert(t);
