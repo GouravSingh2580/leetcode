a448f8068bb2d6be3b0d48fc2974b92e87f295c2
Gourav Singh <49956754+GouravSingh2580@users.noreply.github.com>
2022-08-13 18:59:12 +0530

    Time: 2105 ms (6.12%), Space: 442 MB (5.02%) - LeetHub

--- 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.cpp
+++ 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.cpp
@@ -1,61 +1,29 @@
 class Solution {
 public:
+    unordered_map<string,int>mp;
+    int sz;
     vector<int> findSubstring(string s, vector<string>& words) {
-        int len=0;
-        for(auto x : words) len+=x.size();
-        
-        string tp="";
-        vector<int> ans;
-    
-        vector<pair<string,int>> v; //slide take all windows
-        for(int i=0;i<len;i++) tp+=s[i];
-        
-        v.push_back({tp,0});
-        
-        for(int i=len;i<s.size();i++){
-            tp.erase(tp.begin());
-            tp+=s[i];
-            
-            v.push_back({tp,i-len + 1});
-        }
-        
-        multiset<string> st;
-        for(auto x : words) st.insert(x);
-        
-        multiset<string> st1 = st;
-        
-        int wordlen=words[0].size();
-        for(auto x : v){
-            string y =  x.first;
-         
-            int index = x.second;
-            bool aa=true;
-            string tp1="";
-            
-            for(int i=0;i<wordlen;i++){
-                tp1+=y[i];
-            }
-            
-            if(st.find(tp1)!=st.end()){
-                st.erase(st.find(tp1)); 
-            }
-            else{
-                aa=false;
-            }
-            for(int i=wordlen;i<y.size() and aa;i+=wordlen){
-                tp1 = y.substr(i,wordlen);
-                
-                if(st.find(tp1)!=st.end()){
-                    st.erase(st.find(tp1));
-                }
-                else{
-                    aa=false;
-                }
-            }
-            
-            if(aa) ans.push_back(index);
-            st = st1;
+        vector<int>ans;
+        if(words.size() == 0) return {};
+        sz = words[0].size();
+        int totalSize = sz * words.size();
+        for(auto x:words) mp[x]++;
+        string v;
+        for(int i = 0;i < s.size();i++){
+            v.push_back(s[i]);
+            if(i < totalSize - 1) continue;
+            if(check(v)) ans.push_back(i - totalSize + 1);
+            v.erase(0,1);
         }
         return ans;
+    }
+    
+    bool check(string v){
+        unordered_map<string,int>sub;
+        for(auto x:mp) sub[x.first] = x.second;
+        for(int i = 0;i < v.size();i += sz){
+            if(!sub.count(v.substr(i,sz)) or (--sub[v.substr(i,sz)] < 0)) return 0;
+        }
+        return 1;                                
     }
 };
