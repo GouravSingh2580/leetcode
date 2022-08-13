af6dec4752e693d324cffee7664dccd3d1dcc28d
Gourav Singh <49956754+GouravSingh2580@users.noreply.github.com>
2022-08-13 19:13:24 +0530

    Time: 438 ms (49.98%), Space: 24.7 MB (60.09%) - LeetHub

--- 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.cpp
+++ 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.cpp
@@ -6,16 +6,15 @@ public:
         string t;int x = 0;
         for(int k = i;k<=j-len+1;k+= len){
             string t = s.substr(k,len);
-            if( mp.find(t)== mp.end()) return false;
+            if(mp.count(t)<1) return false;
             else m[t]++;
             if( m[t] == mp[t]) x++;
         }
-        return x == mp.size();
+        return x == mp.size(); 
     }
-    
     vector<int> findSubstring(string s, vector<string>& words) {
         unordered_map<string,int>mp;
-        for(auto & it : words)mp[it]++;
+        for(auto & it : words) mp[it]++;
         int len = words[0].size()*words.size(),i = 0;
         vector<int>res;
         for(int j = len-1;j<s.size();j++,i++){
