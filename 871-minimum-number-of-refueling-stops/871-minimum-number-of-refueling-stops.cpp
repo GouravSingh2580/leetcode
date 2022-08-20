5f95f6cd74bd526c31632372f50a268165fd1e26
Gourav Singh <49956754+GouravSingh2580@users.noreply.github.com>
2022-08-20 19:10:27 +0530

    Time: 35 ms (78.31%), Space: 17.6 MB (12.00%) - LeetHub

--- 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.cpp
+++ 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.cpp
@@ -1,12 +1,25 @@
 class Solution {
 public:
-    int minRefuelStops(int target, int startFuel, vector<vector<int>>& stations) {
-      vector<long long> dp = { startFuel };
-      for (const auto& station : stations) {
-         dp.push_back(0);
-         for (int i = dp.size() - 1; i > 0 && dp[i - 1] >= station[0]; i--) 
-            dp[i] = max(dp[i], dp[i - 1] + station[1]);
-      }
-      return target > dp.back() ? -1 : lower_bound(dp.begin(), dp.end(), target) - dp.begin();
-   }
+    int minRefuelStops(int target, int start, vector<vector<int>>&nums) 
+    {
+        int i=0;
+        int res=0;
+        multiset<int,greater<int>>m;
+        int count=0;
+        while(start<target)
+        {
+            count++;
+            while(i<nums.size()&&nums[i][0]<=start)
+            {
+                m.insert(nums[i++][1]);
+            }
+            if(m.size()==0)
+            {
+                return -1;
+            }
+            start+=*m.begin();
+            m.erase(m.begin());
+        }
+        return count;
+    }
 };
