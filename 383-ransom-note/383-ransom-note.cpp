9626f70c20f0695623900786df350f5712c969ce
Gourav Singh <49956754+GouravSingh2580@users.noreply.github.com>
2022-08-25 12:09:17 +0530

    Time: 50 ms (14.79%), Space: 8.8 MB (39.49%) - LeetHub

--- 383-ransom-note/383-ransom-note.cpp
+++ 383-ransom-note/383-ransom-note.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool canConstruct(string r, string m) {
-        map<char,int>mp;
+        unordered_map<char,int>mp;
         for(auto i:m) mp[i]++;
         
         for(auto i:r){
