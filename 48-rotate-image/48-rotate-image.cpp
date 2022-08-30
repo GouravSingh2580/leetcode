d101811b6ebfda5c985a8a9109a969fe8704e795
Gourav Singh <49956754+GouravSingh2580@users.noreply.github.com>
2022-08-30 22:21:56 +0530

    Time: 11 ms (5.42%), Space: 7.6 MB (33.50%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -2,19 +2,13 @@ class Solution {
 public:
     void rotate(vector<vector<int>>& m) {
         reverse(m.begin(),m.end());
-        
         vector<vector<int>> v;
         
         int k=0;
-        
         while(k<m.size()){
             vector<int>vv;
-            for(auto i:m){
-                vv.push_back(i[k]);
-            }
+            for(auto i:m) vv.push_back(i[k]);
             k++;
-            for(auto i:vv) cout<<i<<" ";
-            cout<<endl;
             v.push_back(vv);
         }
         m=v;
