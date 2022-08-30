6e002b3ddd4c59b13c784b69f225053b5d24d849
Gourav Singh <49956754+GouravSingh2580@users.noreply.github.com>
2022-08-30 22:33:01 +0530

    Time: 8 ms (32.72%), Space: 7.1 MB (53.77%) - LeetHub

--- 8-string-to-integer-atoi/8-string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int myAtoi(string str) {
-        std::stringstream geek(str); 
+        stringstream geek(str); 
         int x=0;
         geek>>x;
         return x;
