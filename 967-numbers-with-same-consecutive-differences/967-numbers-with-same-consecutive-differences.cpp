881afb5f7540ba9791f03f5b1bbd80657f1a035c
Gourav Singh <49956754+GouravSingh2580@users.noreply.github.com>
2022-09-03 20:57:57 +0530

    Time: 6 ms (42.98%), Space: 7.3 MB (82.84%) - LeetHub

--- 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.cpp
+++ 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.cpp
@@ -4,7 +4,7 @@ public:
         if(N == 1) return {0,1,2,3,4,5,6,7,8,9};
 
         deque<int> q = {1,2,3,4,5,6,7,8,9};
-        while(--N > 0) {
+        while(--N) {
             for(int sz = q.size(); sz > 0; sz--) {
                 int n = q.front(); q.pop_front();
                 for (int j = 0; j < 10; ++j)
