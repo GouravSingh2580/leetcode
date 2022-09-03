add6e1c530c54f0e5499711d06b17a9e07f8e623
Gourav Singh <49956754+GouravSingh2580@users.noreply.github.com>
2022-09-03 20:57:13 +0530

    Time: 4 ms (58.93%), Space: 7.3 MB (82.84%) - LeetHub

--- 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.cpp
+++ 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
-    vector<int> ans;
-    void rcheck(int num,int k,int n){
-        if(n==1){
-            ans.push_back(num);
-            return;
+    vector<int> numsSameConsecDiff(int N, int K) {
+        if(N == 1) return {0,1,2,3,4,5,6,7,8,9};
+
+        deque<int> q = {1,2,3,4,5,6,7,8,9};
+        while(--N > 0) {
+            for(int sz = q.size(); sz > 0; sz--) {
+                int n = q.front(); q.pop_front();
+                for (int j = 0; j < 10; ++j)
+                    if(abs((n % 10) - j) == K)
+                        q.push_back(n * 10 + j);
+            }
         }
-        if(num%10-k>=0)
-            rcheck(num*10+(num%10-k),k,n-1);
-        if(k)
-            if(num%10+k<10)
-                rcheck(num*10+(num%10+k),k,n-1);
-    }
-    vector<int> numsSameConsecDiff(int n, int k) {
-        for(int i=1;i<10;i++) rcheck(i,k,n);
-        return ans;
+
+        return vector<int>(q.begin(), q.end());
     }
 };
