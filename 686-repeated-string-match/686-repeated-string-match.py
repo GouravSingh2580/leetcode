aca16481daec947c3b4c692a5971cb4fcb9705a1
Gourav Singh <49956754+GouravSingh2580@users.noreply.github.com>
2022-09-11 18:10:34 +0530

    Time: 56 ms (56.83%), Space: 14 MB (31.83%) - LeetHub

--- 686-repeated-string-match/686-repeated-string-match.py
+++ 686-repeated-string-match/686-repeated-string-match.py
@@ -1,12 +1,16 @@
 class Solution:
     def repeatedStringMatch(self, a: str, b: str) -> int:
-        if not (set(b).issubset(set(a))):
-            return -1
-
-        s = a
+        for i in b:
+            if i not in a:
+                return -1
+        k=0
+        t=a
+        z=""
+        s=a
         for i in range(len(b)//(len(a))+2):
             if b in s:
                 return i+1
             s += a
 
         return -1
+        
