1bd3ae0ca168aa7455dd0a209aec5247352ba1f9
Gourav Singh <49956754+GouravSingh2580@users.noreply.github.com>
2022-09-11 22:31:22 +0530

    Time: 352 ms (85.93%), Space: 31.2 MB (68.25%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.py
+++ 41-first-missing-positive/41-first-missing-positive.py
@@ -3,6 +3,6 @@ class Solution:
         d = defaultdict(int)
         for i in nums:
             d[i] =1
-        for i in range(1,10**8):
+        for i in range(1,10**7):
             if(d[i] == 0):
                 return i
