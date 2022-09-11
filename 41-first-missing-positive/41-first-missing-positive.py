81e50cd815e16b71848b35522971cfb91151f851
Gourav Singh <49956754+GouravSingh2580@users.noreply.github.com>
2022-09-11 22:29:55 +0530

    Time: 821 ms (67.41%), Space: 31.3 MB (68.25%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.py
+++ 41-first-missing-positive/41-first-missing-positive.py
@@ -1,7 +1,9 @@
+from collections import defaultdict as dfd
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
-        k = 1
-        for i in sorted(nums):
-            if i == k:
-                k+=1
-        return k
+        d = dfd(int)
+        for i in nums:
+            d[i] +=1
+        for i in range(1,2**31):
+            if(d[i] == 0):
+                return i
