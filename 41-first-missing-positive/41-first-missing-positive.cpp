0a16ba531f79a775253180ab7a69f8190a91ede3
Gourav Singh <49956754+GouravSingh2580@users.noreply.github.com>
2022-09-11 22:26:07 +0530

    Time: 209 ms (56.54%), Space: 46.6 MB (72.45%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.cpp
+++ 41-first-missing-positive/41-first-missing-positive.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        map<int,int>mp;
+        unordered_map<int,int>mp;
         for(auto i:nums) mp[i]++;
         
         int k=1;
-        while (mp[k])
-            k+=1;
+        while (mp[k]) k+=1;
         
         return k;
     }
