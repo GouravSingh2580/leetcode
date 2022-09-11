d9dcb39bb0d544b617b9a18ccf2ac29f8fab3a74
Gourav Singh <49956754+GouravSingh2580@users.noreply.github.com>
2022-09-11 22:30:06 +0530

    Time: 600 ms (74.03%), Space: 31.2 MB (69.10%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.py
+++ 41-first-missing-positive/41-first-missing-positive.py
@@ -3,7 +3,7 @@ class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
         d = dfd(int)
         for i in nums:
-            d[i] +=1
+            d[i] =1
         for i in range(1,2**31):
             if(d[i] == 0):
                 return i
