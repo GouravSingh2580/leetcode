c96cf44c8ce87d4ae2c9568efe8e6f35e8f17ded
Gourav Singh <49956754+GouravSingh2580@users.noreply.github.com>
2022-09-19 14:28:19 +0530

    Time: 519 ms (21.96%), Space: 44.5 MB (68.87%) - LeetHub

--- 2405-optimal-partition-of-string/2405-optimal-partition-of-string.cpp
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.cpp
@@ -1,27 +1,15 @@
 class Solution {
-    int n;
-    vector<int>dp;
-    int partition(int i, string &s) {
-        if(i == n) return 0;
-        if(dp[i] != -1) return dp[i];
-        
-        vector<int>count(26);
-        int ans = 1e9;
-        for(int j = i; j < n; j++) {
-            if(count[s[j] - 'a'] == 0) {
-                count[s[j] - 'a']++;
-                ans = min(ans, 1 + partition(j + 1, s));
-            } else {
-                break;
-            }
-        }
-        return dp[i] = ans;
-    }
 public:
     int partitionString(string s) {
-        dp.resize(100001,-1);
-        n = s.length();
-        int ans = partition(0, s);
-        return ans;
+        int count = 1;
+        unordered_map<char, int> mp;
+        for(auto &it: s) {
+            if(mp[it] and mp[it] >= 1) {
+                count += 1;
+                mp.clear();
+            }
+            mp[it] += 1;
+        }
+        return count;
     }
 };
