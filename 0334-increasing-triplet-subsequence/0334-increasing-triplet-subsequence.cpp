7b2ffb0d008e8e2fdc6891b13c054e98c20150fd
Gourav Singh <49956754+GouravSingh2580@users.noreply.github.com>
2022-10-12 19:44:31 +0530

    Time: 545 ms (5.09%), Space: 78.3 MB (5.72%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
@@ -1,25 +1,69 @@
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds;
+
+class segment{
+    vector<int> seg;
+    public:
+    segment(int n){
+        seg.assign(4*n,0);
+    }
+    void buildMin(int node,int l,int r,vector<int>&a){
+        if(l==r){
+            seg[node]=a[l];
+            return;
+        }
+        int mid=(l+r)>>1;
+        buildMin(2*node+1,l,mid,a);
+        buildMin(2*node+2,mid+1,r,a);
+        seg[node]=min(seg[2*node+1],seg[2*node+2]);
+    }
+    int queryMin(int node,int l,int r,int ql,int qr){
+        if(ql<=l and r<=qr){
+            return seg[node];
+        }
+        if(ql>r or qr<l){
+            return INT_MAX;
+        }
+        int mid=(l+r)>>1;
+        int lef=queryMin(2*node+1,l,mid,ql,qr);
+        int ri=queryMin(2*node+2,mid+1,r,ql,qr);
+        return min(lef,ri);
+    }
+    void buildMax(int node,int l,int r,vector<int>&a){
+        if(l==r){
+            seg[node]=a[l];
+            return;
+        }
+        int mid=(l+r)>>1;
+        buildMax(2*node+1,l,mid,a);
+        buildMax(2*node+2,mid+1,r,a);
+        seg[node]=max(seg[2*node+1],seg[2*node+2]);
+    }
+    int queryMax(int node,int l,int r,int ql,int qr){
+        if(ql<=l and r<=qr){
+            return seg[node];
+        }
+        if(ql>r or qr<l){
+            return INT_MIN;
+        }
+        int mid=(l+r)>>1;
+        int lef=queryMax(2*node+1,l,mid,ql,qr);
+        int ri=queryMax(2*node+2,mid+1,r,ql,qr);
+        return max(lef,ri);
+    }
+};
 class Solution {
 public:
-    typedef tree<int, null_type, less_equal<int>,rb_tree_tag,tree_order_statistics_node_update> pbd;
     bool increasingTriplet(vector<int>& nums) {
-        vector<int>pre;
-        vector<int>suff;
-        int x=INT_MAX, y=INT_MIN;
-        for(int i=0;i<nums.size();i++){
-            x=min(x,nums[i]);
-            pre.push_back(x);
-        }
-        for(int i=nums.size()-1;i>=0;i--){
-            y=max(y,nums[i]);
-            suff.push_back(y);
-        }
-        reverse(suff.begin(),suff.end());
-        
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]>pre[i] and nums[i]<suff[i]) return true;
+        int n=nums.size();
+        segment *mn=new segment(n+10);
+        segment *mx=new segment(n+10);
+        mn->buildMin(0,0,n-1,nums);
+        mx->buildMax(0,0,n-1,nums);
+        for(int i=0;i<n;i++){
+            int mini=mn->queryMin(0,0,n-1,0,i-1);
+            int maxi=mx->queryMax(0,0,n-1,i+1,n-1);
+            if(mini<nums[i] and nums[i]<maxi){
+                return true;
+            }
         }
         return false;
     }
