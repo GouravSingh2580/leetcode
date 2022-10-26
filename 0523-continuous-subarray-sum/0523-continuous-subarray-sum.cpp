a3897dec9456847d50a21e4c9d339cae966f55e7
Gourav Singh <49956754+GouravSingh2580@users.noreply.github.com>
2022-10-26 19:25:59 +0530

    Time: 227 ms (96.89%), Space: 113.9 MB (26.56%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     bool checkSubarraySum(vector<int>& arr, int k) {
-        if(arr.size()<2) return false;
-        if(k==1) return true;
-        int f=0;
-        for(int i=0;i<arr.size()-1;i++){
-            if(arr[i]==0 && arr[i+1]==0) f=1;
-            if(arr[i]==0 && arr[i+1]%k==0) f=1;
-            if(arr[i]%k==0 && arr[i+1]==0) f=1;
-        }
-        set<int>st;
-        for(auto i:arr) st.insert(i);
-        if(st.size()==1 && arr[0]==k) return true;
-        if(f) return true;
+        // if(arr.size()<2) return false;
+        // if(k==1) return true;
+        // int f=0;
+        // for(int i=0;i<arr.size()-1;i++){
+        //     if(arr[i]==0 && arr[i+1]==0) f=1;
+        //     if(arr[i]==0 && arr[i+1]%k==0) f=1;
+        //     if(arr[i]%k==0 && arr[i+1]==0) f=1;
+        // }
+        // set<int>st;
+        // for(auto i:arr) st.insert(i);
+        // if(st.size()==1 && arr[0]==k) return true;
+        // if(f) return true;
         int n=arr.size();
         unordered_map<int,int>mp;
         mp[0]=-1;
@@ -22,7 +22,7 @@ public:
             if(mp.count(sum%k)){
                 if(i-mp[sum%k]>=2) return true;
             }
-            mp[sum%k]=i;
+            else mp[sum%k]=i;
         }
         return false;
     }
