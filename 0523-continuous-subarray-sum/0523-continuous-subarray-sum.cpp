725010d1d0f30fd86342821ad1de0f62a3dd3611
Gourav Singh <49956754+GouravSingh2580@users.noreply.github.com>
2022-10-26 18:37:57 +0530

    Time: 592 ms (16.23%), Space: 126.7 MB (5.05%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
     bool checkSubarraySum(vector<int>& arr, int k) {
-        
-        
         if(arr.size()<2) return false;
         if(k==1) return true;
         int f=0;
@@ -11,10 +9,8 @@ public:
             if(arr[i]==0 && arr[i+1]%k==0) f=1;
             if(arr[i]%k==0 && arr[i+1]==0) f=1;
         }
-        
         set<int>st;
         for(auto i:arr) st.insert(i);
-        
         if(st.size()==1 && arr[0]==k) return true;
         if(f) return true;
         int n=arr.size();
