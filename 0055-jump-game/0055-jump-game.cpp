3ea8b9aa667af45147d3518be449ab4e62183df1
Gourav Singh <49956754+GouravSingh2580@users.noreply.github.com>
2022-12-26 18:21:36 +0530

    Time: 62 ms (96.13%), Space: 48.5 MB (48.80%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    // To solve this problem, we can use a greedy approach. The idea is to keep track of the maximum reachable index at each step, and return true if we can reach the last index.
     bool canJump(vector<int>& nums) {
         int maxReachableIndex = 0;
         for (int i = 0; i < nums.size(); i++) {
