2ee6c2e84a72147b5f6befd2a28761babe8d1534
Gourav Singh <49956754+GouravSingh2580@users.noreply.github.com>
2023-01-03 20:08:22 +0530

    Time: 438 ms (5.18%), Space: 37.9 MB (5.80%) - LeetHub

---
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool sidhi(string s){
+        string t=s;
+        sort(s.begin(),s.end());
+        return t==s;
+    }
+    int minDeletionSize(vector<string>& strs) {
+        map<int,vector<char>>v;
+        for(auto i:strs){
+            int c=0;
+            for(auto j:i){
+                v[c++].push_back(j);
+            }
+        }
+        int res=0;
+        for(auto i:v){
+            string s;
+            for(auto j:i.second) s+=j;
+            
+            if(!sidhi(s)) res++;
+        }
+        return res;
+    }
+};
