2cbbf9c683a0001fae51b54559b73fefb50f87d1
Gourav Singh <49956754+GouravSingh2580@users.noreply.github.com>
2023-01-03 20:09:16 +0530

    Time: 180 ms (6.42%), Space: 30.4 MB (5.80%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
@@ -7,16 +7,16 @@ public:
     }
     int minDeletionSize(vector<string>& strs) {
         unordered_map<int,vector<char>>v;
-        for(auto i:strs){
+        for(auto &i:strs){
             int c=0;
-            for(auto j:i){
+            for(auto &j:i){
                 v[c++].push_back(j);
             }
         }
         int res=0;
-        for(auto i:v){
+        for(auto &i:v){
             string s;
-            for(auto j:i.second) s+=j;
+            for(auto &j:i.second) s+=j;
             
             if(!sidhi(s)) res++;
         }
