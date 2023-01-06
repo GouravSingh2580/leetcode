342cb8af3f4458495b324c0af7e4aa65632114f2
Gourav Singh <49956754+GouravSingh2580@users.noreply.github.com>
2023-01-07 00:04:58 +0530

    Time: 2033 ms (12.25%), Space: 25.3 MB (51.02%) - LeetHub

---
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def maxIceCream(self, A, coins):
+        A.sort()
+        for i, a in enumerate(A):
+            coins -= a
+            if coins < 0:
+                return i
+        return len(A)
