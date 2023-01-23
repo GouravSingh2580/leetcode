0eb152276f2fda4c1cb6472bb1f34f7e6a55e504
Gourav Singh <49956754+GouravSingh2580@users.noreply.github.com>
2023-01-23 09:50:06 +0530

    Time: 293 ms (32.68%), Space: 67.6 MB (51.01%) - LeetHub

---
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -0,0 +1,30 @@
+template<typename T> ostream& operator<< (ostream& out, const vector<T>& v) {
+        out << "{";
+        size_t last = v.size() - 1;
+        for(size_t i = 0; i < v.size(); ++i) {
+            out << v[i];
+            if (i != last) 
+                out << ", ";
+        }
+        out << "}";
+        return out;
+    }
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        if(n==1) return 1;
+        map<int,int>mp;
+        map<int,vector<int>>dp;
+        for(auto &i:trust){
+            mp[i[0]]++;
+            dp[i[1]].push_back(i[0]);
+        }
+        for(auto &i:trust){
+            // cout<<i.first<<" "<<i.second<<endl;
+            if(mp.count(i[1])<1) {
+                if(dp[i[1]].size()==n-1) return i[1];
+            }
+        }
+        return -1;
+    }
+};
